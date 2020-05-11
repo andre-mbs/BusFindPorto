02e74674a8c28b3ec581cb520aa26452a7c12fd9
Andre <andre.mb.santos@gmail.com>
2020-05-11 17:38:28 +0100

    Search functionality

--- BusFindPorto/src/main/java/pt/ua/busfind/BusFindController.java
+++ BusFindPorto/src/main/java/pt/ua/busfind/BusFindController.java
@@ -1,9 +1,15 @@
 package pt.ua.busfind;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 /**
  *
@@ -15,13 +21,45 @@ public class BusFindController {
     @Autowired
     private BusRepository br;
     
-    @RequestMapping("busList")
+    @RequestMapping({"/", "busList"})
     public String busList(Model model) {
         System.out.println("\nFrom the demo controller: busList\n");
         
         Iterable<Bus> busList = br.findAll();
         model.addAttribute("busList", busList);
         
+        Set<String> ids = new HashSet<>();
+        for (Bus b : busList) {
+            ids.add(b.getNode_id());
+        }
+        model.addAttribute("ids", ids);
+        
+        return "dashboard";
+    }
+    
+    @PostMapping({"/", "busList"})
+    public String busList(Model model, @RequestParam(name = "nodeID", required = false, defaultValue = "all") String nodeID) {
+        System.out.println("\nFrom the demo controller: busList\n");
+
+        Iterable<Bus> busList = br.findAll();
+
+        if (nodeID.equals("all")) {
+            model.addAttribute("busList", busList);
+        } else {
+            List<Bus> busListFiltered = new ArrayList<>();
+            for (Bus b : busList) {
+                if(b.getNode_id().equals(nodeID))
+                    busListFiltered.add(b);
+            }
+            model.addAttribute("busList", busListFiltered);
+        }
+
+        Set<String> ids = new HashSet<>();
+        for (Bus b : busList) {
+            ids.add(b.getNode_id());
+        }
+        model.addAttribute("ids", ids);
+
         return "dashboard";
     }
     
